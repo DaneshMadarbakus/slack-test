3d69d0fef61d848628bfb1ca9eb6c10d0cc79970
Danesh Madarbakus <daneshplt@hotmail.co.uk>
2022-12-20 04:00:19 +0000

    working functionality

--- app/index.js
+++ app/index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const cors = require("cors");
-const bodyParser = require('body-parser')
-const app = express();
 const { CookieJar } = require('tough-cookie');
 const { wrapper } = require('axios-cookiejar-support');
 
 const axios = require("axios");
 var FormData = require('form-data');
-var data = new FormData();
+
+const bodyParser = require('body-parser')
+const app = express();
 
 app.use(cors())
 app.use(bodyParser.json())
@@ -19,7 +19,9 @@ const slackMagicAxiosClient = wrapper(axios.create({
 }));
 
 app.post("/login", (req, res) => {
-  console.log("Danesh /login");
+  console.log("Logging in", req.body.email);
+
+  const data = new FormData();
   data.append('email', req.body.email);
 
   slackMagicAxiosClient({
@@ -31,18 +33,19 @@ app.post("/login", (req, res) => {
     data: data
   })
     .then(response => {
-
-      console.log(JSON.stringify(response.data));
+      console.log(response.data)
     })
     .catch(err => {
       console.log(err);
     });
 
-  res.sendStatus(200)
+  res.sendStatus(200);
 })
 
 app.post("/confirm-email", (req, res) => {
-  console.log("/confirm-email")
+  console.log("Confirming email with code")
+
+  const data = new FormData();
   data.append('code', req.body.code);
   data.append('email', req.body.email);
 
@@ -55,32 +58,30 @@ app.post("/confirm-email", (req, res) => {
     data: data
   };
 
-  console.log("/confirm-email 1", config);
-
   slackMagicAxiosClient(config)
     .then(response => {
-      console.log("/confirm-email 2")
-      console.log(JSON.stringify(response.data));
       response.headers['set-cookie'];
-      res.sendStatus(200)
+      res.sendStatus(200);
     })
     .catch(err => {
       console.log(err);
       res.sendStatus(500);
     });
-
 })
 
 app.get("/workspaces", (req, res) => {
+  console.log("Retrieving workspaces");
+
   slackMagicAxiosClient({
     method: 'post',
     url: 'https://slack.com/api/signin.findWorkspaces',
   })
     .then(response => {
       const workspaces = response.data.current_teams[0].teams.map(team => {
-        return { id: team.id, name: team.name, url: team.url }
+        return { id: team.id, name: team.name, url: team.url, magicUrl: team.magic_login_url, magicLoginCode: team.magic_login_code }
       })
-      res.send({ status: 200, workspaces });
+
+      res.send({ status: 200, workspaces });;
     })
     .catch(err => {
       console.log(err);
@@ -88,33 +89,61 @@ app.get("/workspaces", (req, res) => {
     });
 })
 
+let token = "";
+
+app.post("/join-workspace", async (req, res) => {
+  console.log("Joining workspace with magic url");
+
+  await slackMagicAxiosClient({
+    method: 'get',
+    url: `${req.body.magicUrl}`
+  }).then(response => {
+    console.log("Slack API token: ", response.data.match(/\"api_token\":\"(.*?)\"/)[1]);
+    token = response.data.match(/\"api_token\":\"(.*?)\"/)[1];
+  }).catch(err => {
+    console.log(JSON.stringify(err));
+    res.sendStatus(500);
+  })
+
+  await slackMagicAxiosClient({
+    method: 'get',
+    url: `${req.body.url}${req.body.magicLoginCode}`
+  }).catch(err => {
+    console.log(JSON.stringify(err));
+    res.sendStatus(500);
+  })
+})
+
 app.post("/send-message", (req, res) => {
+  console.log("Sending message");
 
-  // const message = req.body.message;
-  // console.log("Danesh /send-message 0")
+  const message = req.body.message;
+  const channel = req.body.channel;
+  const workspaceURL = req.body.workspaceUrl;
+  const data = new FormData();
 
-  // data.append('blocks', `[{"type":"rich_text","elements":[{"type":"rich_text_section","elements":[{"type":"text","text":"${message}"}]}]}]`);
+  data.append('blocks', `[{"type":"rich_text","elements":[{"type":"rich_text_section","elements":[{"type":"text","text":"${message}"}]}]}]`);
+  data.append('token', token);
+  data.append('channel', channel);
 
-  // var config = {
-  //   method: 'post',
-  //   url: 'https://techpeople-world.slack.com/api/chat.postMessage',
-  //   headers: {
+  var config = {
+    method: 'post',
+    url: `${workspaceURL}api/chat.postMessage`,
+    headers: {
+      ...data.getHeaders()
+    },
+    data: data
+  };
 
-  //     ...data.getHeaders()
-  //   },
-  //   data: data
-  // };
-
-  // slackMagicAxiosClient(config)
-  //   .then(function (response) {
-  //     console.log(JSON.stringify(response.data));
-  //   })
-  //   .catch(function (error) {
-  //     console.log(error);
-  //   });
-
-
-  // res.sendStatus(200)
+  slackMagicAxiosClient(config)
+    .then((response) => {
+      console.log(response.data);
+      res.sendStatus(200);
+    })
+    .catch((err) => {
+      console.log(JSON.stringify(err));
+      res.sendStatus(500);
+    });
 });
 
 app.listen(8001, () => {
--- frontend/src/views/Homepage.js
+++ frontend/src/views/Homepage.js
@@ -1,22 +1,18 @@
 import React, { useState } from "react";
 
-
-
-
-
-
-
 const Homepage = () => {
   const [email, setEmail] = useState("");
   const [code, setCode] = useState("");
   const [message, setMessage] = useState("");
   const [workspaces, setWorkspaces] = useState([]);
+  const [selectedWorkspace, setSelectedWorkspace] = useState({});
+  const [channel, setChannel] = useState([]);
 
   const sendSlackMessage = async () => {
     const response = await fetch("http://localhost:8001/send-message", {
       headers: { "Content-type": "application/json" },
       method: "POST",
-      body: JSON.stringify({ message })
+      body: JSON.stringify({ message, channel, workspaceUrl: selectedWorkspace.url })
     });
 
     console.log("sendSlackMessage response: ", response);
@@ -33,27 +29,38 @@ const Homepage = () => {
   }
 
   const confirmEmail = async () => {
+    // console.log("Confirm email", code, email)
     try {
-      const response = await fetch("http://localhost:8001/confirm-email", {
+      await fetch("http://localhost:8001/confirm-email", {
         headers: { "Content-type": "application/json" },
         method: "POST",
         body: JSON.stringify({ code, email })
       });
 
-      console.log("confirmEmail response: ", response);
+      const workspaces = await fetch("http://localhost:8001/workspaces", {
+        headers: { "Content-type": "application/json" }
+      });
 
-      const workspaces = await fetch("http://localhost:8001/workspaces",
-        {
-          headers: { "Content-type": "application/json" }
-        });
+      console.log("workspace 1", workspaces);
 
-      const workspacesJSON = workspaces.json()
+      const workspacesJSON = await workspaces.json();
 
-      setWorkspaces(workspacesJSON);
-      console.log(workspacesJSON);
+      console.log("workspace 2", workspacesJSON);
+
+      setWorkspaces(workspacesJSON.workspaces);
+      console.log(workspacesJSON.workspaces);
     } catch (err) {
       console.log("ERROR", err)
     }
+  }
+
+  const joinWorkspace = async () => {
+    const response = await fetch("http://localhost:8001/join-workspace", {
+      headers: { "Content-type": "application/json" },
+      method: "POST",
+      body: JSON.stringify({ magicUrl: selectedWorkspace.magicUrl, url: selectedWorkspace.url, magicLoginCode: selectedWorkspace.magicLoginCode })
+    });
+    console.log("Danesh enterWorkspace", response);
   }
 
   return <main style={{
@@ -80,8 +87,27 @@ const Homepage = () => {
         cursor: "pointer"
       }}
       onClick={() => {
-        confirmEmail(code)
+        confirmEmail()
       }}>ENTER CODE</button>
+
+    {workspaces.map(x => {
+      return (<button onClick={() => setSelectedWorkspace(x)} key={`${x.name}`}>{x.name}</button>)
+    })}
+
+    {selectedWorkspace.name ? <p>You are in the {selectedWorkspace.name} workspace</p> : null}
+    <button
+      style={{
+        cursor: "pointer"
+      }}
+      onClick={() => {
+        joinWorkspace()
+      }}>JOIN WORKSPACE</button>
+
+    <label>
+      Enter channel to message
+      <input type="text" onChange={(e) => { setChannel(e.target.value) }} />
+    </label>
+
 
     <input type="text" onChange={(e) => { setMessage(e.target.value) }} />
     <button
@@ -89,10 +115,8 @@ const Homepage = () => {
         cursor: "pointer"
       }}
       onClick={() => {
-        sendSlackMessage(message)
+        sendSlackMessage()
       }}>SEND MESSAGE</button>
-
-    {workspaces.length > 0 ? JSON.stringify(workspaces) : null}
   </main>
 }
 
